ece4d8db4d22eb7e7703a02ba24b40f787fe877c
Murzify <murzify@gmail.com>
2024-04-16 11:26:36 +0500

    convention plugin for the unit tests

---
+++ build-logic/convention/src/main/kotlin/foodies.unit-test.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    "testImplementation"(libs.mockito.core)
+    "testImplementation"(libs.mockito.kotlin)
+    "testImplementation"(libs.junit)
+    "testImplementation"(libs.coroutines.test)
+}
